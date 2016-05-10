70a44dc0d5a5847dfabe7e7c5bf5895dec6c74df
Emanuele Spies <emanuelespies@gmail.com>
2016-05-10 18:31:56 -0300

    [rm] unnecessary file [up] styles and rss limit [up] the quote url to good reads

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,27 +9,25 @@ var esGoodQuotes = {
 	callQuotes: function() { "use strict";					  
 		var url = "https://www.goodreads.com/quotes/list_rss/32122757-emanuele";
 		$.ajax({
-				url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+				url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=-1&callback=?&q=' + encodeURIComponent(url),
 				dataType   : 'json',
 				success: function (data) {
 					var quotesLength 	= data.responseData.feed.entries.length,
 						randomNr		=  Math.floor(Math.random() * (quotesLength - 0)) + 0,
-						content			= data.responseData.feed.entries[randomNr].content;
+						content			= data.responseData.feed.entries[randomNr].content,
+						link			= data.responseData.feed.entries[randomNr].link;
 
 						var appendContent = content.split('--');
-
-						// just to check if it is really working
-						window.console.log(randomNr);
 
 					// remove loading element
 					esGoodQuotes.loading();
 
-					$('.loaded-quotes').append("<li class='list-group-item'><em>" + appendContent[0] + "</em><small class='text-right'>" + appendContent[1] + "</small>" + "</li>");
+					$('.loaded-quotes').append("<p class='list-group-item'><em>" + appendContent[0] + "</em><small class='text-right'><a target='_blank' alt='Click to see on Good Reads Page' href='" + link + "'>" + appendContent[1] + "</a></small>" + "</p>");
 					$('.loaded-quotes').show();
 				},
 				error: function(error){
 					window.console.log("Cannot get data " + error);
-					$('.container').addClass('alert alert-danger').html("Sorry, we have a problem. Maybe you will have lucky in the next time.");
+					$('.loaded-quotes').toggleClass('well alert alert-danger').html("Sorry, we had a problem. Maybe youá better ll have luck in the next time.");
 				}
 		});
 	},
